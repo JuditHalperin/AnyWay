380165844c1ae70b521e9fef779d94b36da96eaf
Judit <yuhalper@g.jct.ac.il>
2020-10-16 13:52:58 +0300

    Initial code + Welcome6589

--- dotNET5781_6589_5401/dotNET5781_00_6589_5401/Program5401.cs
+++ dotNET5781_6589_5401/dotNET5781_00_6589_5401/Program5401.cs
@@ -16,7 +16,7 @@ namespace dotNET5781_00_6589_5401
         }
 
         static partial void Welcome6589();
-
+        
         private static void Welcome5401()
         {
             Console.Write("Enter your name:");
---
+++ dotNET5781_6589_5401/dotNET5781_00_6589_5401/Program6589.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_6589_5401
+{
+    partial class Program
+    {
+        static partial void Welcome6589()
+        {
+            Console.WriteLine("Finally did it!");
+        }
+
+    }
+}
