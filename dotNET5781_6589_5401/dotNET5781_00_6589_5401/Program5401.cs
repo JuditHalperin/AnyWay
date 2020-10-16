1cb6ddec157ea8066d2de26d90981f40b2f9b1ec
אסנת <askahana@g.jct.ac.il>
2020-10-16 12:01:10 +0300

    initial code+Welcome5401

---
+++ dotNET5781_6589_5401/dotNET5781_00_6589_5401/Program5401.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_6589_5401
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5401();
+            Welcome6589();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6589();
+
+        private static void Welcome5401()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+    }
+}
