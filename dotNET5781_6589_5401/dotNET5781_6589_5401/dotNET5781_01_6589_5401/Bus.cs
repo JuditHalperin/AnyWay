8b0659c3138ca33edc89ee1f602ad4800d7c8457
Judit <yuhalper@g.jct.ac.il>
2020-10-25 14:59:12 +0200

    begining of bus class

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_01_6589_5401/Bus.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_01_6589_5401
+{
+    class Bus
+    {
+        static private Random rand = new Random();
+
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            private set
+            {
+                if (value.Length < 7 || value.Length > 8) // wrong length
+                    return; // exeption...
+
+                int num;
+                bool flag = int.TryParse(value, out num);
+                if (!flag) // convert failed
+                    return;
+
+                if (num < 1000000 || num > 100000000)  // not all digits
+                    return;
+
+                string tmp = Convert.ToString(num);
+
+                if (dateOfBegining.Year < 2018 && tmp.Length == 7) // 7 digits
+                {
+                    tmp = tmp.Insert(2, "-");
+                    tmp = tmp.Insert(6, "-");
+                }
+
+                else if (dateOfBegining.Year > 2018 && tmp.Length == 8) // 8 digits
+                {
+                    tmp = tmp.Insert(3, "-");
+                    tmp = tmp.Insert(6, "-");
+                }
+
+                else // length does not fit the year
+                    return;
+
+                id = tmp;
+            }
+        }
+
+        private DateTime dateOfBegining;
+        public DateTime DateOfBegining
+        {
+            get { return dateOfBegining; }
+            private set { dateOfBegining = value; }
+        }
+
+        private DateTime dateOfLastTreat;
+        public DateTime DateOfLastTreat
+        {
+            get { return dateOfLastTreat; }
+            private set { dateOfLastTreat = value; }
+        }
+
+        private float totalKm;
+        public float TotalKm
+        {
+            get { return totalKm; }
+            private set { totalKm = value; }
+        }
+
+        private float kmSinceFueling;
+        public float KmSinceFueled
+        {
+            get { return kmSinceFueling; }
+            private set { kmSinceFueling = value; }
+        }
+
+        private float kmSinceTreated;
+        public float KmSinceTreated
+        {
+            get { return kmSinceFueling; }
+            private set { kmSinceFueling = value; }
+        }
+
+        public Bus(DateTime date, string id) // constructor
+        {
+            DateOfBegining = date;
+            Id = id;
+            TotalKm = 0;
+            KmSinceFueled = 0;
+            KmSinceTreated = 0;
+        }
+
+        public void drive() // if possible - drive a bus
+        {
+            string msg;
+            float km = kmOfDrive();
+            bool possible = isValid(out msg, km);
+
+            if (possible)
+                updateKm(km);
+            else
+                Console.WriteLine("The bus cannot drive: " + msg);
+        }
+        public float kmOfDrive() // random length of drive
+        {
+            return rand.Next(0, 1200);
+        }
+
+        public bool isValid(out string msg, float km = 0) // test if the bus can drive
+        {
+            TimeSpan timeSinceLastTreat = DateTime.Now - DateOfLastTreat;
+            if (timeSinceLastTreat.TotalDays > 365)
+            {
+                msg = "the bus needs a traet because the last one was a year ago";
+                return false;
+            }
+
+            if (KmSinceTreated + km > 20000)
+            {
+                msg = "the bus needs a traet because it drived more than 20,000 km";
+                return false;
+            }
+
+            if(KmSinceFueled + km > 1200)
+            {
+                msg = "the bus needs to get fueled";
+                return false;
+            }
+
+            msg = "everything's alright";
+            return true;
+
+        }
+
+        public void updateKm(float km) // update the km fields after drive
+        {
+            TotalKm += km;
+            KmSinceFueled += km;
+            KmSinceTreated += km;
+        }
+    
+    }
+}
---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_01_6589_5401/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_01_6589_5401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // תאריך בודק תקינות?
+            // לבנות בנאי ריק לאוטובוס?
+            // אם נוצר אוטוטבוס שגוי?
+
+            DateTime d = new DateTime(2015, 7, 8);
+            string i = "123456a";
+
+            Bus bus = new Bus(d, i);
+
+            Console.WriteLine(bus.Id);
+            Console.WriteLine(bus.DateOfBegining);
+            Console.ReadKey();
+        }
+    }
+}
