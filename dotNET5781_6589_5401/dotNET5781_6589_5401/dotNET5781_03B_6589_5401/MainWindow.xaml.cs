beb9174a73eba7406c60aebb28ff60797974a25e
Judit <yuhalper@g.jct.ac.il>
2020-11-26 17:22:36 +0200

    c

--- dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/MainWindow.xaml.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/MainWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace dotNET5781_03B_6589_5401
 
         private void FuelButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void BusesList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
