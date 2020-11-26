5ee67d585a9ffa648ae5199bc541cf422c7bb7fd
אסנת <askahana@g.jct.ac.il>
2020-11-26 16:47:17 +0200

    ttt

--- dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/MainWindow.xaml.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/MainWindow.xaml.cs
@@ -46,5 +46,31 @@ namespace dotNET5781_03B_6589_5401
             (BusesList.SelectedItems as Bus).fuel();
         }
 
+
+    }
+    public class StateToBool : IValueConverter
+    {
+        public object Convert(
+          object value,
+          Type targetType,
+          object parameter,
+          CultureInfo culture)
+        {
+            State stateValue = (State)value;
+            if (stateValue == State.canDrive)
+                return true;
+
+            else
+                return false;
+        }
+
+        public object ConvertBack(
+          object value,
+          Type targetType,
+          object parameter,
+          CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
