a9cc44b9066e7a99ef09ecdd534b6cecddb22166
Judit <yuhalper@g.jct.ac.il>
2020-12-03 11:30:12 +0200

    remove

--- dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/AddBus.xaml.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/dotNET5781_03B_6589_5401/AddBus.xaml.cs
@@ -51,5 +51,36 @@ namespace dotNET5781_03B_6589_5401
         {
             Close();
         }
+
+        private void OnlyNumbers_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox text = sender as TextBox;
+            if (text == null) return;
+            if (e == null) return;
+
+            // alloe Enter and Tab keys
+            if (e.Key == Key.Return || e.Key == Key.Enter || e.Key == Key.Tab)
+                return;
+
+            // allow list of system keys (add other key here if you want to allow)
+            if (e.Key == Key.Escape || e.Key == Key.Back || e.Key == Key.Delete ||
+            e.Key == Key.CapsLock || e.Key == Key.LeftShift || e.Key == Key.Home
+           || e.Key == Key.End || e.Key == Key.Insert || e.Key == Key.Down || e.Key == Key.Right)
+                return;
+
+            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+
+            // allow control system keys
+            if (Char.IsControl(c)) return;
+
+            // allow digits (without Shift or Alt)
+            if (Char.IsDigit(c))
+                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt)))
+                    return; // let this key be written inside the textbox
+
+            // forbid letters and signs (#,$, %, ...)
+            e.Handled = true; // ignore this key. mark event as handled, will not be routed to other controls
+            return;
+        }
     }
 }
