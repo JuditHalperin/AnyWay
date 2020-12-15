6dd501f8e2fb4819fb3e33771063b4e385611a30
Judit <yuhalper@g.jct.ac.il>
2020-12-15 16:09:20 +0200

    ds

--- dotNET5781_6589_5401/dotNET5781_6589_5401/DS/DataSource.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DS/DataSource.cs
@@ -10,12 +10,28 @@ namespace DS
 {
     public static class DataSource
     {
-        public static List<WindDirection> directions;//
         public static List<User> users;
+        public static List<Station> stations;
+        public static List<LineStation> lineStations;
+        public static List<TwoFollowingStations> followingStationsCouples;
+        public static List<Bus> buses;
+        public static List<DrivingBus> drivingBuses;
+        public static List<Line> lines;
+        public static List<DrivingLine> drivingLines;
+
+        public static List<WindDirection> directions;//
 
         static DataSource()
         {
             users = new List<User>();
+            stations = new List<Station>();
+            lineStations = new List<LineStation>();
+            followingStationsCouples = new List<TwoFollowingStations>();
+            buses = new List<Bus>();
+            drivingBuses = new List<DrivingBus>();
+            lines = new List<Line>();
+            drivingLines = new List<DrivingLine>();
+
             directions = new List<WindDirection>();//
             directions.Add(new WindDirection());//
         }
