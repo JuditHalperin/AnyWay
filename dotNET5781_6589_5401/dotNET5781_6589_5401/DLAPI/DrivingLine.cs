e3cbad47b88813de31632f7658884e110f802295
Judit <yuhalper@g.jct.ac.il>
2020-12-15 15:43:52 +0200

    classes

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DLAPI/DrivingLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    class DrivingLine
+    {
+        public int NumberLine { get; set; }
+        public DateTime Start { get; set; }
+        public int Frequency { get; set; }
+        public DateTime End { get; set; }
+    }
+}
