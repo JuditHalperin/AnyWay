dbb4db7b527b1ead05e1a3e2cc772440c342ebd0
Judit <yuhalper@g.jct.ac.il>
2020-12-15 10:56:18 +0200

    bo

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/BL/BL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    internal sealed class MyBL : IBL
+    {
+
+    }
+}
---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/BL/State.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BO
+{
+    public enum State
+    {
+        canDrive, cannotDrive, driving, gettingFueled, gettingTreated
+    }
+}
