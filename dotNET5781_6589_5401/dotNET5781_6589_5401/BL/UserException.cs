be5338fe63f6cdea67f77cf83dacb3ae606d5c81
Judit <yuhalper@g.jct.ac.il>
2020-12-16 16:35:11 +0200

    UserException

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/BL/UserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    [Serializable]
+    public class UserException : Exception
+    {
+        // constructors:
+        public UserException() : base() { }
+        public UserException(string message) : base(message) { }
+        public UserException(string message, Exception inner) : base(message, inner) { }
+        protected UserException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        /// <summary>
+        /// print the exception
+        /// </summary>
+        /// <returns>specific massage</returns>
+        override public string ToString() { return Message; }
+    }
+}
