bc8cd24bd0bf2f56e448710adabc31e9e0e1e665
Judit <yuhalper@g.jct.ac.il>
2020-12-16 13:28:23 +0200

    clone

--- dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/Cloning.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/Cloning.cs
@@ -9,23 +9,23 @@ namespace DL
 {
     static class Cloning // one of three
     {
-        internal static IClonable Clone(this IClonable original)//דרך שניה - בונוס (יש להשתמש בממשק)
-        {
-            IClonable target = (IClonable)Activator.CreateInstance(original.GetType());
-            //...
-            return target;
-        }
+        //internal static IClonable Clone(this IClonable original)//דרך שניה - בונוס (יש להשתמש בממשק)
+        //{
+        //    IClonable target = (IClonable)Activator.CreateInstance(original.GetType());
+        //    //...
+        //    return target;
+        //}
 
-        internal static T Clone<T>(this T original)//דרך שלישית - בונוס
-        {
-            T target = (T)Activator.CreateInstance(original.GetType());
-            //...
-            return target;
-        }
+        //internal static T Clone<T>(this T original)//דרך שלישית - בונוס
+        //{
+        //    T target = (T)Activator.CreateInstance(original.GetType());
+        //    //...
+        //    return target;
+        //}
 
-        internal static WindDirection Clone(this WindDirection original) //דרך ראשונה
+        internal static T Clone<T>(this T original)
         {
-            WindDirection target = new WindDirection();
+            T target = new WindDirection();
             target.direction = original.direction;
             return target;
         }
--- dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/DalObject.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/DalObject.cs
@@ -24,6 +24,8 @@ namespace DL
 
         void addBus(Bus bus)
         {
+            if (!DS.DataSource.Buses.Exists(item => item.LicensePlate == bus.LicensePlate))
+                throw new BusException("The bus already exsits.");
             DS.DataSource.Buses.Add(bus);
         }
         void removeBus(Bus bus)
@@ -33,10 +35,10 @@ namespace DL
         }
         void updateBus(Bus bus)
         {
-            Bus bust;
+            Bus busT;
             try
             {
-                bust = (from busTemp in DS.DataSource.Buses//Find the bus for update.
+                busT = (from busTemp in DS.DataSource.Buses//Find the bus for update.
                                  where busTemp.LicensePlate == bus.LicensePlate
                                  select busTemp).First();
             }
@@ -44,7 +46,7 @@ namespace DL
             {
                 throw new BusException("There is not exsits bus with same license of the bus for update.");
             }
-            DS.DataSource.Buses.Remove(bust);//Remove the old bus
+            DS.DataSource.Buses.Remove(busT);//Remove the old bus
             DS.DataSource.Buses.Add(bus);//Add the update bus
         }
         Bus getBus(string licensePlate)
@@ -89,10 +91,10 @@ namespace DL
         }
         void updateLine(Line line)
         {
-            Line linet;
+            Line lineT;
             try
             {
-                linet = (from lineTemp in DS.DataSource.Lines//Find the bus for update.
+                lineT = (from lineTemp in DS.DataSource.Lines//Find the bus for update.
                              where lineTemp.ThisSerial == line.ThisSerial
                              select lineTemp).First();
             }
@@ -100,8 +102,8 @@ namespace DL
             {
                 throw new LineException("There is not exsits line with same serial of the line for update.");
             }
-            DS.DataSource.Lines.Remove(linet);//Remove the old bus
-            DS.DataSource.Lines.Add(linet);//Add the update bus
+            DS.DataSource.Lines.Remove(lineT);//Remove the old bus
+            DS.DataSource.Lines.Add(line);//Add the update bus
         }
         Line getLine(int serial)
         {
@@ -120,7 +122,7 @@ namespace DL
         }
         IEnumerable<Line> GetLines()
         {
-            return DS.DataSource.Lines;
+            return DS.DataSource.Lines.Clone();
         }
         IEnumerable<Line> GetLines(Predicate<Line> condition)
         {
@@ -131,58 +133,6 @@ namespace DL
         }
 
         #endregion
-
-        #region stations
-
-        void addStation(Station station);
-        void removeStation(Station station);
-        void updateStation(Station station);
-        Bus getStation(int id);
-        IEnumerable<Station> GetStations();
-        IEnumerable<Station> GetStations(Predicate<Station> condition);
-
-        #endregion
-
-        #region lineStations
-
-        void addLineStation(LineStation lineStation);
-        void removeLineStation(LineStation lineStation);
-        void updateLineStation(LineStation lineStation);
-        Bus getLineStation(int numberLine, int id);
-        IEnumerable<Station> GetLineStations();
-        IEnumerable<Station> GetLineStations(Predicate<LineStation> condition);
-
-        #endregion
-
-        #region followingStations
-
-        void addTwoFollowingStations(TwoFollowingStations lineStation);
-        void removeTwoFollowingStations(TwoFollowingStations lineStation);
-        void updateTwoFollowingStations(TwoFollowingStations lineStation);
-        Bus getTwoFollowingStations(int numberLine, int id);
-        IEnumerable<Station> GetFollowingStations();
-        IEnumerable<Station> GetFollowingStations(Predicate<TwoFollowingStations> condition);
-
-        #endregion
-
-
-        //static Random rnd = new Random(DateTime.Now.Millisecond);
-        //double temperature;
-
-        //public double GetTemparture(int day)
-        //{
-        //    temperature = rnd.NextDouble() * 50 - 10;
-        //    temperature += rnd.NextDouble() * 10 - 5;//122
-        //    return temperature;
-        //}
-
-        //public WindDirection GetWindDirection(int day)
-        //{
-        //    WindDirection direction = DataSource.directions.Find(d => true);
-        //    var directions = (WindDirections[])Enum.GetValues(typeof(WindDirections));
-        //    direction.direction = directions[rnd.Next(0, directions.Length)];
-
-        //    return direction.Clone();
-        //}
+       
     }
 }
