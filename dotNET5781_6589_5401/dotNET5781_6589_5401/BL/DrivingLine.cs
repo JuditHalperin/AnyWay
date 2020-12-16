e19dc39838b59b60aabc3afd0f2610d193bb19ee
Judit <yuhalper@g.jct.ac.il>
2020-12-16 11:24:35 +0200

    DrivingLine

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/BL/DrivingLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class DrivingLine
+    {
+        private int numberLine; public int NumberLine
+        {
+            get { return numberLine; }
+            private set { numberLine = value; }
+        }
+        private DateTime start; public DateTime Start
+        {
+            get { return start; }
+            private set { start = value; }
+        }
+        private int frequency; public int Frequency
+        {
+            get { return frequency; }
+            private set { frequency = value; }
+        }
+        private DateTime end; public DateTime End
+        {
+            get { return end; }
+            private set { end = value; }
+        }
+
+        public DrivingLine(int numberLine, DateTime start, int frequency, DateTime end)
+        {
+            NumberLine = numberLine;
+            Start = start;
+            Frequency = frequency;
+            End = end;
+        }
+    }
+}
