50020068917f32c811fb8fa597d2ed7d4f19feef
Judit <yuhalper@g.jct.ac.il>
2020-12-16 13:52:42 +0200

    bb

--- dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/DalObject.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DalObject/DalObject.cs
@@ -11,27 +11,28 @@ namespace DL
 {
     public class DalObject : IDAL
     {
-        #region singelton
+        #region Singelton
 
         static readonly DalObject instance = new DalObject();
+        public static DalObject Instance => instance;
+
         static DalObject() { }
         DalObject() { }
-        public static DalObject Instance => instance;
 
         #endregion
 
-        #region buses
+        #region Buses
 
         void addBus(Bus bus)
         {
             if (!DS.DataSource.Buses.Exists(item => item.LicensePlate == bus.LicensePlate))
-                throw new BusException("The bus already exsits.");
+                throw new BusException("The bus already exists.");
             DS.DataSource.Buses.Add(bus);
         }
         void removeBus(Bus bus)
         {
             if(!DS.DataSource.Buses.Remove(bus))
-                throw new BusException("The bus is not exsits");           
+                throw new BusException("The bus does not exist.");           
         }
         void updateBus(Bus bus)
         {
@@ -78,16 +79,18 @@ namespace DL
 
         #endregion
 
-        #region lines
+        #region Lines
 
         void addLine(Line line)
         {
+            if (!DS.DataSource.Lines.Exists(item => item.ThisSerial == line.ThisSerial))
+                throw new LineException("The line already exists.");
             DS.DataSource.Lines.Add(line);
         }
         void removeLine(Line line)
         {
             if (!DS.DataSource.Lines.Remove(line))
-                throw new LineException("The line is not exsits");
+                throw new LineException("The line does not exist.");
         }
         void updateLine(Line line)
         {
@@ -98,7 +101,7 @@ namespace DL
                              where lineTemp.ThisSerial == line.ThisSerial
                              select lineTemp).First();
             }
-            catch
+            catch (ArgumentNullException)
             {
                 throw new LineException("There is not exsits line with same serial of the line for update.");
             }
@@ -133,6 +136,103 @@ namespace DL
         }
 
         #endregion
-       
+
+        #region Stations
+
+        void addStation(Station station)
+        {
+            Station clonedStation = station.Clone();
+            if (DS.DataSource.Stations.Exists(item => item.ID == station.ID))
+                throw new StationException("The station already exists.");
+            DS.DataSource.Stations.Add(station);
+        }
+        void removeStation(Station station)
+        {
+            if (!DS.DataSource.Stations.Remove(station))
+                throw new StationException("The station does not exist.");
+        }
+        void updateStation(Station station)
+        {
+            Station stationT;
+            try
+            {
+                stationT = (from item in DS.DataSource.Stations
+                            where item.ID == station.ID
+                            select item).First();
+            }
+            catch (ArgumentNullException)
+            {
+                throw new LineException("The station does not exist.");
+            }
+            DS.DataSource.Stations.Remove(stationT); // remove the old station
+            DS.DataSource.Stations.Add(station); // add the updated station
+        }
+        Station getStation(int id)
+        {
+            Station station;
+            try
+            {
+                station = (from item in DS.DataSource.Stations
+                           where item.ID == id
+                           select item).First();
+            }
+            catch (ArgumentNullException)
+            {
+                throw new LineException("The station does not exist.");
+            }
+            return station;
+        }
+        IEnumerable<Station> GetStations()
+        {
+            return DS.DataSource.Stations;
+        }
+        IEnumerable<Station> GetStations(Predicate<Station> condition)
+        {
+
+        }
+
+        #endregion
+
+        #region LineStations
+
+        void addLineStation(LineStation lineStation);
+        void removeLineStation(LineStation lineStation);
+        void updateLineStation(LineStation lineStation);
+        LineStation getLineStation(int numberLine, int id);
+        IEnumerable<Station> GetLineStations();
+        IEnumerable<Station> GetLineStations(Predicate<LineStation> condition);
+
+        #endregion
+
+        #region FollowingStations
+
+        void addTwoFollowingStations(TwoFollowingStations twoFollowingStations);
+        void removeTwoFollowingStations(TwoFollowingStations twoFollowingStations);
+        void updateTwoFollowingStations(TwoFollowingStations twoFollowingStations);
+        TwoFollowingStations getTwoFollowingStations(int firstStationID, int secondStationID);
+        IEnumerable<Station> GetFollowingStations();
+        IEnumerable<Station> GetFollowingStations(Predicate<TwoFollowingStations> condition);
+
+        #endregion
+
+        #region DrivingBuses
+
+        void addDrivingBus(DrivingBus drivingBus);
+        void removeDrivingBus(DrivingBus drivingBus);
+        DrivingBus getDrivingBus(int thisSerial, string licensePlate, int line, DateTime start);
+        IEnumerable<Station> GetDrivingBuses();
+        IEnumerable<Station> GetDrivingBuses(Predicate<DrivingBus> condition);
+
+        #endregion
+
+        #region DrivingLines
+
+        void addDrivingLine(DrivingLine drivingLine);
+        void removeDrivingLine(DrivingLine drivingLine);
+        DrivingLine getDrivingLine(int numberLine, DateTime start);
+        IEnumerable<Station> GetDrivingLines();
+        IEnumerable<Station> GetDrivingLines(Predicate<DrivingLine> condition);
+
+        #endregion
     }
 }
