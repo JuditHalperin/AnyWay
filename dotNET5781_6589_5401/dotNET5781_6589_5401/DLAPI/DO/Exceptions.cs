67a1234d490744957827a14b03a0d2338e5d7e37
Judit <yuhalper@g.jct.ac.il>
2020-12-19 18:03:19 +0200

    cs

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/DLAPI/DO/Exceptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    [Serializable]
+    public class BusException : Exception
+    {
+        public BusException() : base() { }
+        public BusException(string message) : base(message) { }
+        public BusException(string message, Exception inner) : base(message, inner) { }
+        protected BusException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class LineException : Exception
+    {
+        public LineException() : base() { }
+        public LineException(string message) : base(message) { }
+        public LineException(string message, Exception inner) : base(message, inner) { }
+        protected LineException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class StationException : Exception
+    {
+        public StationException() : base() { }
+        public StationException(string message) : base(message) { }
+        public StationException(string message, Exception inner) : base(message, inner) { }
+        protected StationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class UserException : Exception
+    {
+        public UserException() : base() { }
+        public UserException(string message) : base(message) { }
+        public UserException(string message, Exception inner) : base(message, inner) { }
+        protected UserException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
