ee9dab01891826d689c68b96502d8a9507e6e85c
Judit <yuhalper@g.jct.ac.il>
2020-12-24 11:26:06 +0200

    lines

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Lines/AddLine.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BLAPI;
+using PO;
+using BO;
+
+namespace PL.Lines
+{
+    /// <summary>
+    /// Interaction logic for AddLine.xaml
+    /// </summary>
+    public partial class AddLine : Window
+    {
+        static IBL bl;
+
+        public AddLine()
+        {
+            InitializeComponent();
+            bl = BlFactory.GetBl();
+            RegionsList.ItemsSource = new List<Regions> { Regions.General, Regions.North, Regions.South, Regions.Center, Regions.Jerusalem };
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
