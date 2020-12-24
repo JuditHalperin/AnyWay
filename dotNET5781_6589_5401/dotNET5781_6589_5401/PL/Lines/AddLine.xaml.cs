af4b700726129debb3a17530bb935af47f2792ec
Judit <yuhalper@g.jct.ac.il>
2020-12-24 12:18:14 +0200

    line

--- dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Lines/AddLine.xaml.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Lines/AddLine.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 using BLAPI;
 using PO;
 using BO;
@@ -23,12 +24,17 @@ namespace PL.Lines
     public partial class AddLine : Window
     {
         static IBL bl;
+        ObservableCollection<Station> path = new ObservableCollection<Station>();
 
         public AddLine()
         {
             InitializeComponent();
             bl = BlFactory.GetBl();
             RegionsList.ItemsSource = new List<Regions> { Regions.General, Regions.North, Regions.South, Regions.Center, Regions.Jerusalem };
+            StationsList.ItemsSource = bl.GetStations();
+            StationsList.Text = ((Station)StationsList.SelectedItem).ID + " - " + ((Station)StationsList.SelectedItem).Name;
+            ChosenStations.ItemsSource = path;
+            Ok.IsEnabled = false;
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
@@ -37,6 +43,29 @@ namespace PL.Lines
         }
 
         private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int line;
+                if (LineNumber.Text.Length == 0 || !int.TryParse(LineNumber.Text, out line))
+                    throw new InvalidInputException("Invalid format of line number.");
+                if (path.Count() < 2)
+                    throw new InvalidInputException("Path line should be consisted of at least 2 stations.");
+                bl.addLine(new BO.Line() { NumberLine = line, Region = (Regions)RegionsList.SelectedItem, Path = convertToLineStationsList(path) });
+            }
+            catch (InvalidInputException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StationsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(!path.Contains((Station)StationsList.SelectedItem))
+                path.Add((Station)StationsList.SelectedItem);
+        }
+
+        IEnumerable<LineStation> convertToLineStationsList(ObservableCollection<Station> path)
         {
 
         }
