8f9fae343a3016c7d3fd2f355ca5a47b87b91d75
Judit <yuhalper@g.jct.ac.il>
2021-01-03 12:54:21 +0200

    DistanceToOldStation

---
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Stations/DistanceToOldStation.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PO;
+using BO;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for DistanceToOldStation.xaml
+    /// </summary>
+    public partial class DistanceToOldStation : Window
+    {
+        static public int Distance = 0;
+
+        public DistanceToOldStation()
+        {
+            InitializeComponent();
+            Massage.Content = "You have changed the location of the station. \n" +
+                "Please enter the distance to the old location: \n" +
+                "(use + or - to mark the direction)";
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!int.TryParse(DistanceTextBox.Text, out Distance))
+                    throw new InvalidInputException("Worng format of distance.");
+                if (Distance == 0 && (Distance < -240000 || Distance > 240000)) // outside of Israel
+                    throw new InvalidInputException("Invalid distance.");
+                Close();
+            }
+            catch(InvalidInputException ex)
+            { 
+                MessageBox.Show(ex.Message);
+                new DistanceToOldStation().ShowDialog();
+                Close();
+            }
+        }
+    }
+}
