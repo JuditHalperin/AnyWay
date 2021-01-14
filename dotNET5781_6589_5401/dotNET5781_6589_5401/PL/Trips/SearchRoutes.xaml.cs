2aae795f068fc3a0ef4559329977de3b6f158139
Judit <yuhalper@g.jct.ac.il>
2021-01-14 15:22:03 +0200

    worker

--- dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Trips/SearchRoutes.xaml.cs
+++ dotNET5781_6589_5401/dotNET5781_6589_5401/PL/Trips/SearchRoutes.xaml.cs
@@ -1,19 +1,13 @@
-﻿using System;
+﻿using BLAPI;
+using BO;
+using PO;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using BLAPI;
-using PO;
-using BO;
 
 namespace PL
 {
@@ -24,6 +18,8 @@ namespace PL
     {
         static IBL bl;
         string username;
+        BackgroundWorker worker;
+        bool selectionChanged;
 
         public SearchRoutes(string name)
         {
@@ -35,6 +31,13 @@ namespace PL
             Time.DataContext = dateTime;
             dateTime.worker.RunWorkerAsync();
 
+            worker = new BackgroundWorker();
+            worker.DoWork += RediscoverRoutes;
+            worker.ProgressChanged += PresentRoutse;
+            worker.RunWorkerCompleted += done;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
             username = name;
 
             IEnumerable<Station> stations = bl.GetStations();
@@ -42,50 +45,73 @@ namespace PL
             TargetStation.ItemsSource = stations;
         }
 
-        private void visibilities(object sender, SelectionChangedEventArgs e)
+        private void RediscoverRoutes(object sender, DoWorkEventArgs e)
         {
-            if (SourceStation.SelectedItem != null && TargetStation.SelectedItem != null)
-                if (((Station)SourceStation.SelectedItem).ID == ((Station)TargetStation.SelectedItem).ID)
+            selectionChanged = false;
+            if (selectionChanged)
+                worker.CancelAsync();
+
+            while (true) // while the selection is not changed
+            {
+                worker.ReportProgress(0);
+                Thread.Sleep(1000); // second
+            }
+        }
+
+        private void PresentRoutse(object sender, ProgressChangedEventArgs e)
+        {
+            var trips = from DrivingBus drivingBus in bl.getPassengerTrips(((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
+                        let timeTillArrival = bl.timeTillArrivalToSource(drivingBus, ((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
+                        let timeOfJourney = bl.durationTripBetweenStations(drivingBus.NumberLine, ((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
+                        where timeTillArrival.Seconds != -1 // not relevant
+                        select new // anonymous variable for PL trip
+                        {
+                            Line = drivingBus.NumberLine,
+                            TimeTillArrival = timeTillArrival.ToString(@"hh\:mm\:ss"),
+                            TimeOfJourney = timeOfJourney.ToString(@"hh\:mm\:ss"),
+                            TotalTime = (timeTillArrival + timeOfJourney).ToString(@"hh\:mm\:ss")
+                        };
+
+            if (trips.Count() == 0)
+            {
+                NoLines.Visibility = Visibility.Visible;
+                Titles.Visibility = Visibility.Hidden;
+                Trips.Visibility = Visibility.Hidden;
+            }
+            else // show first 10 sorted results
+            {
+                Trips.ItemsSource = trips.Take(10).OrderBy(item => item.TotalTime);
+                NoLines.Visibility = Visibility.Hidden;
+                Titles.Visibility = Visibility.Visible;
+                Trips.Visibility = Visibility.Visible;
+            }
+        }
+
+        void done(object sender, RunWorkerCompletedEventArgs e)
+        {
+            return;
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectionChanged = true;
+            if (SourceStation.SelectedItem != null && TargetStation.SelectedItem != null) // both stations selected
+                if (((Station)SourceStation.SelectedItem).ID == ((Station)TargetStation.SelectedItem).ID) // same station
                 {
                     SameStation.Visibility = Visibility.Visible;
                     NoLines.Visibility = Visibility.Hidden;
                     Titles.Visibility = Visibility.Hidden;
                     Trips.Visibility = Visibility.Hidden;
                 }
-                else
+                else // present routes
                 {
                     SameStation.Visibility = Visibility.Hidden;
-
-                    var trips = from DrivingBus drivingBus in bl.getPassengerTrips(((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
-                                let timeTillArrival = bl.timeTillArrivalToSource(drivingBus, ((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
-                                let timeOfJourney = bl.durationTripBetweenStations(drivingBus.NumberLine, ((Station)SourceStation.SelectedItem).ID, ((Station)TargetStation.SelectedItem).ID)
-                                where timeTillArrival.Seconds != -1 // not relevant
-                                select new // anonymous variable for PL trip
-                                {
-                                    Line = drivingBus.NumberLine,
-                                    TimeTillArrival = timeTillArrival.ToString(@"hh\:mm\:ss"),
-                                    TimeOfJourney = timeOfJourney.ToString(@"hh\:mm\:ss"),
-                                    TotalTime = (timeTillArrival + timeOfJourney).ToString(@"hh\:mm\:ss")
-                                };
-
-                    if (trips.Count() == 0)
-                    {
-                        NoLines.Visibility = Visibility.Visible;
-                        Titles.Visibility = Visibility.Hidden;
-                        Trips.Visibility = Visibility.Hidden;
-                    }
-                    else
-                    {
-                        Trips.ItemsSource = trips.Take(10).OrderBy(item => item.TotalTime); // show first 10 results
-                        NoLines.Visibility = Visibility.Hidden;
-                        Titles.Visibility = Visibility.Visible;
-                        Trips.Visibility = Visibility.Visible;
-                    }
+                    worker.RunWorkerAsync();
                 }
         }
 
         private void Back_MouseDown(object sender, MouseButtonEventArgs e)
-        {
+        {           
             new PassengerWindow(username).Show();
             Close();
         }
